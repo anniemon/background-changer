f565decbb7dab0f43b370ed5172e5d9874d6c362
seojung94 <73627443+seojung94@users.noreply.github.com>
2021-02-17 00:12:39 +0900

    first finished....

--- hexcolor.js
+++ hexcolor.js
@@ -2,24 +2,46 @@ const hex = document.querySelector(".colorname"),
     color = hex.querySelector("h1"),
     button = hex.querySelector("button");
 
-const randomNumber = "randomNumber";
-randomNumber = Math.floor(Math.random()*10)
 
+const RANDOMPICK_LS = "colorname";
 
-
-function Number(randomNumber) {
-    return Math.floor(Math.random()*10)
+function saveColorName(text){
+    localStorage.setItem(RANDOMPICK_LS, text);
 }
+
+function pick() {
+    const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+    const sol = array[Math.floor(Math.random()*array.length)];
+    return sol; // array에서 randomly pick
+}
+
+function combine(){
+    const picked = `${pick()}${pick()}${pick()}${pick()}${pick()}${pick()}`;
+    return picked;
+}
+
 //0~9까지의 숫자와 A~F까지의 알파벳이 랜덤하게 구성되어 이루는 6자리 코드
 
-console.log();
-
 function handleSumit(){
-    color.innerText = `HEX COLOR :#${randomNumber}`;
-    //컬러를 바꿔야함....여기서
+    const currentValue = combine();
+    paintColor(currentValue);
+    saveColorName(currentValue);
+}
+
+function paintColor(text){
+    color.innerText = `HEX COLOR :#${text}`;
+    hex.style.backgroundColor = `#${text}`;
+}
+
+function loadColorName(){
+    const loadedColor = localStorage.getItem(RANDOMPICK_LS);
+    if(loadedColor !== null){
+        paintColor(loadedColor);
+    } 
 }
 
 function init() {
+    loadColorName();
     button.addEventListener("click", handleSumit);
 }
 
